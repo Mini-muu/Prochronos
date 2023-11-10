7b3c2e49e9d61e41794c2107e86831e24bdb350d
StrikerDrake <149331550+DrakeDevCoder@users.noreply.github.com>
2023-11-10 19:14:46 +0100

    Aggiunta Inventario e oggetti - Marco (Drake)
    [AGGIUNTE]
    • Inventario
      - UI (Slot)
      - Lista Oggetti (e quantità)
    • Sistema Oggetti
      - Icona
      - Nome
      - Menu per la creazione di oggetti

---
+++ Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Data", menuName = "Data/Item")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+}
