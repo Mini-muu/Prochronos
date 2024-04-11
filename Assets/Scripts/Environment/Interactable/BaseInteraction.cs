dd4b47c311466174ec7f3450ff198874b873022b
StrikerDrake <149331550+DrakeDevCoder@users.noreply.github.com>
2024-04-11 11:04:23 +0200

    Implementazione base interazioni, correzioni, implementazione HealthBar
    [AGGIUNTE]
    - Implementazione della classe base per le varie future interazioni con l'ambiente
    - Nuova implementazione della HealthBar con sprite temporanei
    - Implementazione di un Serializabledictionary per l'inventory
    
    [CORREZIONI]
    - Piccole correzioni sul database dell'inventory, ItemData e GameData
    - Correzioni minori nella scena "Tutorial"
    
    [ALTRO]
    - Riordino cartelle ScriptableObject per compatibilità con il database dell'inventory

---
+++ Assets/Scripts/Environment/Interactable/BaseInteraction.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BaseInteraction : MonoBehaviour
+{
+    public bool IsKeyInteractionNeeded;
+    [Space]
+    [Header("Temporaneo")]
+    public KeyCode interactionKey;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            if (IsKeyInteractionNeeded && Input.GetKeyDown(interactionKey))
+                ExecuteInteraction();
+            else
+                ExecuteInteraction();
+        }
+    }
+
+    public virtual void ExecuteInteraction() 
+    {
+
+    }
+
+    //Type interaction
+    //Monoliti -> Interagibile
+    //Cartelli -> Interagibile
+    //Oggetti ambientali -> Interagibile
+    //Albero Mercante -> Attaccabile (Pesante)
+    //Falò -> Entrare nell'area specificata
+}
--- Assets/Scripts/SaveAndLoad/GameData.cs
+++ Assets/Scripts/SaveAndLoad/GameData.cs
@@ -1,10 +1,17 @@
+using System.Collections.Generic;
+
 [System.Serializable]
 public class GameData
 {
     public int currency;
 
+    public SerializableDictionary<string, int> inventory;
+
     public GameData()
     {
-        currency = 0;   
+        currency = 0;
+
+        inventory = new SerializableDictionary<string, int>();
+
     }
 }
